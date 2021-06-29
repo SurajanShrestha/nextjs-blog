d39064035ea67ca63938191a2bb1697ae71edb6c
Surajan Shrestha <surajanshrestha35@gmail.com>
2021-06-29 10:32:27 +0545

    First Commit

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+// pages/404.js
+export default function Custom404() {
+    return (
+        <div>
+            <h1>404 - Page Not Found</h1>
+            <small>This is a custom 404 page created by <a href="https://github.com/SurajanShrestha" target="_blank">Surajan Shrestha</a></small>
+        </div>
+    );
+}
